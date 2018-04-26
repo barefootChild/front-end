152841bb62eb238494f971c5078efcd8fa2e692c
zhaoyongsheng <zhaoyongsheng1@xiaomi.com>
2018-04-26 20:19:41 +0800

    feat: add new module of sentry

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="root_page">
-    hello my baby!
+    {{msg}}
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello my baby!'
+    }
+  },
+  mounted () {
+    console.log(this.message.msg)
+  }
+}
+</script>
 <style>
 .root_page {
   height: 1800px;
--- src/main.js
+++ src/main.js
@@ -5,6 +5,12 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import Raven from 'raven-js'
+import RavenVue from 'raven-js/plugins/vue'
+
+Raven.config('https://f4dff36d48a54457917434b05f0c8123@sentry.io/1196764')
+  .addPlugin(RavenVue, Vue)
+  .install()
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
